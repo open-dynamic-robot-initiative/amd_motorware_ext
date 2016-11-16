91e9d669fec7595a1c4a4d7f9ac90a18683d988d
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2016-11-16 15:55:01 +0100

    Add utils functions from dual_motor_torque_ctrl

---
+++ include/amd_motorware_ext/utils.h
@@ -0,0 +1,41 @@
+#ifndef AMDMWEXT_UTILS_H_
+#define AMDMWEXT_UTILS_H_
+
+#include "sw/modules/math/src/32b/math.h"
+#include "sw/modules/est/src/32b/est.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+//! \brief Remove offset from position in an overflow safe way
+//!
+//! Remove the offset from the position and preserve the range of the result
+//! (position is periodic and lies within [-max_pos, +max_pos].  The
+//! computations are done such that integer overflows are avoided, i.e. this
+//! also works if values are near to the max. value of the used IQ values.
+//! Note that this is important as pos_max may go up to 100 while the max. for
+//! IQ24 is 127!
+//!
+//! \param position The actual position in range [-pos_max, +pos_max]
+//! \param offset The offset between actual and desired zero position
+//! \param pos_max The maximum position.  When going beyond that value, the
+//!                position rolls over to -max_pos.
+//! \returns Position with offset removed.
+_iq removePositionOffset(_iq position, _iq offset, _iq pos_max);
+
+
+//! \brief Computes Torque in Nm
+//!
+//! This is a modified version of the function from user.c which does not need a
+//! CTRL_Handle.
+_iq computeTorque_Nm(EST_Handle estHandle, MATH_vec2 Idq_pu,
+		const _iq torque_Flux_sf, const _iq torque_Ls_sf);
+
+
+#ifdef __cplusplus
+}
+#endif // extern "C"
+
+#endif  // ifndef AMDMWEXT_UTILS_H_
---
+++ src/utils.c
@@ -0,0 +1,62 @@
+#include <amd_motorware_ext/utils.h>
+
+_iq removePositionOffset(_iq position, _iq offset, _iq pos_max)
+{
+	// Remove offset in a way that preserves the range and does not exceed the
+	// max. value in subterms of the calculations (which would cause integer
+	// overflows and make it explode).
+	if (offset < 0)
+	{
+		_iq off_p_max = offset + pos_max;
+		if (position < off_p_max) {
+			position -= offset;
+		} else {
+			position = (position - pos_max) - off_p_max;
+		}
+	}
+	else if (offset > 0)
+	{
+		_iq off_m_max = offset - pos_max;
+		if (position > off_m_max) {
+			position -= offset;
+		} else {
+			position = (position + pos_max) - off_m_max;
+		}
+	}
+
+	/*
+	 * The above code does the same thing than this one, but avoids overflows by
+	 * ensuring that subterms never get greater than max_pos.
+	 *
+		position = positon - offset
+		if (position > mrev_max) {
+			position -= 2 * mrev_max;
+		} else if (position < -mrev_max) {
+			position += 2 * mrev_max;
+		}
+	*/
+
+	return position;
+}
+
+
+_iq computeTorque_Nm(EST_Handle estHandle, MATH_vec2 Idq_pu,
+		const _iq torque_Flux_sf, const _iq torque_Ls_sf)
+{
+  _iq Flux_pu = EST_getFlux_pu(estHandle);
+  _iq Id_pu = Idq_pu.value[0];
+  _iq Iq_pu = Idq_pu.value[1];
+  _iq Ld_minus_Lq_pu = _IQ30toIQ(
+		  EST_getLs_d_pu(estHandle) - EST_getLs_q_pu(estHandle));
+  _iq Torque_Flux_Iq_Nm = _IQmpy(
+		  _IQmpy(Flux_pu, Iq_pu),
+		  torque_Flux_sf);
+  _iq Torque_Ls_Id_Iq_Nm = _IQmpy(
+		  _IQmpy(
+				  _IQmpy(Ld_minus_Lq_pu, Id_pu),
+				  Iq_pu),
+		  torque_Ls_sf);
+  _iq Torque_Nm = Torque_Flux_Iq_Nm + Torque_Ls_Id_Iq_Nm;
+
+  return(Torque_Nm);
+} // end of USER_computeTorque_Nm() function
